a74930b968c99046b0bd9f6a09994c71767fdb9d
allenpeng05 <157744889+allenpeng05@users.noreply.github.com>
2026-07-13 16:02:11 -0700

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.py
@@ -0,0 +1,24 @@
+# backtracking algorithm
+# save the global result, as well as the current subset we are building
+# for num in nums, we either include it or don't include it
+# when index == len(nums), we have made a decision about every number
+# for num in nums, we either include it or don't include it
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        current = []
+
+        def backtrack(index):
+            if index == len(nums):
+                result.append(current.copy())
+                return
+            
+            current.append(nums[index])
+            backtrack(index + 1)
+
+            current.pop()
+            backtrack(index + 1)
+        
+        backtrack(0)
+        return result
+        
