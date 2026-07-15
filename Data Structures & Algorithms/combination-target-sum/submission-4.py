3780e51a66eadb8a5609820b165af5128484611d
allenpeng05 <157744889+allenpeng05@users.noreply.github.com>
2026-07-15 15:34:32 -0700

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.py
@@ -0,0 +1,35 @@
+# similar solution to subset, similar backtracking algorithm
+# difference is that same number may be chosen multiple times
+
+# base cases: we have used all the numbers, we find a correct combination(ie
+# current_target = 0), we go over(ie current_target < 0)
+
+# keep a current_target to track which numbers we used
+# when we backtrack, we can either skip the number, or use it
+# we only move index forward when we skip the number because we are able to use it
+# multiple times
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        current = []
+        result = []
+
+        def backtrack(index, current_target):
+            if index == len(nums):
+                return
+            if current_target == 0:
+                result.append(current.copy())
+                return
+            if current_target < 0:
+                return
+            
+            current.append(nums[index])
+            backtrack(index, current_target - nums[index])
+
+            current.pop()
+            backtrack(index + 1, current_target)
+        
+        backtrack(0, target)
+        return result
+
+            
+
